40cf175eef73e9791878c5087ed89af6a01e0da5
Giorgi Isaiashvili <giorgi.isaiashvili@gmail.com>
2019-09-11 14:22:00 +0400

    Added author and book entities without relation

---
+++ Library.Core/Entities/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Core.Entities
+{
+    public class Author
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfbirth { get; set; }
+        public string Genre { get; set; }
+    }
+}
